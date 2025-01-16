114e6c8679dedd12a9ceb62a11bc9170c414ef09
MartinPettersen <searchingtheskyfor@gmail.com>
2025-01-16 12:44:51 +0100

    add Aria labels

--- src/app/components/category/category.component.ts
+++ src/app/components/category/category.component.ts
@@ -4,13 +4,18 @@ import { CommonModule } from '@angular/common';
 import { RobotComponent } from "../robot/robot.component";
 
 @Component({
-  selector: 'app-category',
-  imports: [CommonModule, RobotComponent],
-  templateUrl: './category.component.html',
-  styleUrl: './category.component.css'
+    selector: 'app-category',
+    imports: [CommonModule, RobotComponent],
+    templateUrl: './category.component.html',
+    styleUrl: './category.component.css'
 })
 export class CategoryComponent {
     @Input() categoryLabel!: string;
     @Input() category!: RobotPet[];
+    showRobots = true;
+
+    toggleRobots() {
+        this.showRobots = !this.showRobots;
+    }
 
 }
