5108ecae49e760300d3f4cca5cf9fa07d4b28f84
MartinPettersen <searchingtheskyfor@gmail.com>
2025-01-20 13:04:43 +0100

    change shoppingcart over to localstorage

--- src/app/services/shoppingcart.service.ts
+++ src/app/services/shoppingcart.service.ts
@@ -8,13 +8,17 @@ import { BehaviorSubject, Observable } from 'rxjs';
 export class ShoppingcartService {
 
 
+private cartKey = "shoppingCart"
+private cart: BehaviorSubject<RobotPet[]>;
 
-private cart: BehaviorSubject<RobotPet[]> = new BehaviorSubject<RobotPet[]>([]);
-
-cart$: Observable<RobotPet[]> = this.cart.asObservable();
+cart$: Observable<RobotPet[]>;
 
 constructor() {
     console.log(`ShoppingcartService instance created: ${Math.random()}`);
+    const savedCart = JSON.parse(localStorage.getItem(this.cartKey) || '[]')
+    this.cart = new BehaviorSubject<RobotPet[]>(savedCart);
+    this.cart$ = this.cart.asObservable();
+
 }
 getProducts$(): Observable < RobotPet[] > {
     console.log("getting cart in service", this.cart.getValue())
@@ -23,9 +27,13 @@ getProducts$(): Observable < RobotPet[] > {
 
 addProduct(robot: RobotPet): void {
     console.log("adding robot in service", robot)
-    const tempCart = this.cart.getValue();
-    const updatedCart = [...tempCart, robot]
+    const updatedCart = [...this.cart.getValue(), robot];
     this.cart.next(updatedCart);
+    localStorage.setItem(this.cartKey, JSON.stringify(updatedCart))
+
+    //const tempCart = this.cart.getValue();
+    //const updatedCart = [...tempCart, robot]
+    //this.cart.next(updatedCart);
     console.log("Cart state after update:", updatedCart);
     console.log("Cart after update:", this.cart.getValue());
 
